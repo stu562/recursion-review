9bd32bbdccd20a537ec680b4fb679989e9ade17d
stu562 <stu562@gmail.com>
2019-01-02 12:35:39 -0800

    implement parseJSON with errors

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,51 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  // [{"x":[10,null,null,null]},3,"false",false]
+
+  if (json === "null") {
+    return null;
+  } else if (json === "true") {
+    return true;
+  } else if (json === "false") {
+    return false;
+  } else if(json[0] === '"'){
+    return json.slice(1, json.length - 1);
+  } else if(json[0] === '[') {
+    var result = [];
+    var currentIndex = 1;
+    while(currentIndex < json.length - 1) {
+      var nextElement = findNextElement(json, currentIndex);
+      result.push(parseJSON(nextElement));
+      currentIndex += nextElement.length;
+    }
+    return result;
+  } else if(json[0] === '{') {
+    var result = {};
+    var currentIndex = 1;
+    while(currentIndex < json.length - 1) {
+      var nextElement = findNextElement(json, currentIndex);
+      result.push(parseJSON(nextElement));
+      currentIndex += nextElement.length;
+    }
+    return result;
+  } else {
+    //return parseFloat(json);
+  }
 };
+var findNextElement = function(json, start) {
+    var openBrackets = 0;
+    var i = start;
+    while(true){
+      var char = json[i];
+      if (char === '{' || char === '[') {
+        openBrackets++;  
+      } else if (char === '}' || char === ']') {
+        openBrackets--;  
+      } else if (openBrackets === 0 && (char === ',' || char === ']' || char === '}')) {
+        break;
+      }
+      i++;
+    }
+    return json.slice(start, i);
+  }
