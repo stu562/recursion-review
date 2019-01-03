5a2b17b2f49d63092c66eb0908bae0ee353e68d8
stu562 <stu562@gmail.com>
2019-01-02 20:37:22 -0800

    Make further changes to parseString, still buggy. Double quotes exist

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,28 +4,29 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // [{"x":[10,null,null,null]},3,"false",false]
-  if (json === "null") {
+  json = json.trim();
+  if (json === 'null') {
     return null;
-  } else if (json === "true") {
+  } else if (json === 'true') {
     return true;
-  } else if (json === "false") {
+  } else if (json === 'false') {
     return false;
   } else if (json[0] === '"') {
-    return parseString(json, 0);
+    return parseString(json, -1);
   } else if (json[0] === '[') {
   // check this section
     var result = [];
     var currentIndex = 1;
-    while(currentIndex < json.length - 1) {
+    while (currentIndex < json.length - 1) {
       var nextElement = findNextElement(json, currentIndex);
       result.push(parseJSON(nextElement.trim()));
       currentIndex += nextElement.length + 1;
     }
     return result;
-  } else if(json[0] === '{') {
+  } else if (json[0] === '{') {
     var result = {};
     var currentIndex = 1;
-    while(currentIndex < json.length - 1) {
+    while (currentIndex < json.length - 1) {
       var prop = parseString(json, currentIndex);
       currentIndex += prop.length + 3; //accounts for ':'
       var value = findNextElement(json, currentIndex);
@@ -41,7 +42,7 @@ var parseJSON = function(json) {
 var findNextElement = function(json, start) {
   var openBrackets = 0;
   var i = start;
-  while(true){
+  while (true) {
     var char = json[i];
     if (openBrackets === 0 && (char === ',' || char === ']' || char === '}')) {
       break;
@@ -53,19 +54,29 @@ var findNextElement = function(json, start) {
     i++;
   }
   return json.slice(start, i);
-}
+};
 
 var parseString = function(json, start) {
   var prevBackslash = false;
   var prevDoubleBackslash = false;
   var i = start + 1;
-  var escapeChars = ["/", "b", "f", "n", "r", "t"];
-  while(true){
+  while (true) {
+    if(json[i] === ' '){
+      i++;
+    } else if (json[i] === '"'){
+      i++;
+      break;
+    } else {
+      break;
+    }
+  }
+  var escapeChars = ['/', 'b', 'f', 'n', 'r', 't'];
+  while (true) {
     var char = json[i];
-    if (char === "\\" && (!prevBackslash || prevDoubleBackslash)) {
+    if (char === '\\' && (!prevBackslash || prevDoubleBackslash)) {
       prevBackslash = true;
       prevDoubleBackslash = false;
-    } else if (char === "\\" && prevBackslash) {
+    } else if (char === '\\' && prevBackslash) {
       prevDoubleBackslash = true;
       prevBackslash = false; 
     } else if (prevBackslash && escapeChars.includes(char)) {
