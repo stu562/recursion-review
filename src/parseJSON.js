b077010815868f07925b1063d133d47f8f84bc88
stu562 <stu562@gmail.com>
2019-01-02 16:50:25 -0800

    implement parseString helper function

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -36,18 +36,40 @@ var parseJSON = function(json) {
   }
 };
 var findNextElement = function(json, start) {
-    var openBrackets = 0;
-    var i = start;
-    while(true){
-      var char = json[i];
-      if (char === '{' || char === '[') {
-        openBrackets++;  
-      } else if (char === '}' || char === ']') {
-        openBrackets--;  
-      } else if (openBrackets === 0 && (char === ',' || char === ']' || char === '}')) {
-        break;
-      }
-      i++;
+  var openBrackets = 0;
+  var i = start;
+  while(true){
+    var char = json[i];
+    if (char === '{' || char === '[') {
+      openBrackets++;  
+    } else if (char === '}' || char === ']') {
+      openBrackets--;  
+    } else if (openBrackets === 0 && (char === ',' || char === ']' || char === '}')) {
+      break;
     }
-    return json.slice(start, i);
+    i++;
   }
+  return json.slice(start, i);
+}
+
+var parseString = function(json, start) {
+  var prevBackslash = false;
+  var prevDoubleBackslash = false;
+  var i = start + 1;
+  var escapeChars = ["/", "b", "f", "n", "r", "t"];
+  while(true){
+    var char = json[i];
+    if (char === "\\" && !prevBackslash) {
+      prevBackslash = true;
+    } else if (char === "\\" && prevBackslash) {
+      prevDoubleBackslash = true; 
+    } else if (prevBackslash && escapeChars.includes(char)) {
+      throw new SyntaxError();
+    } else if (prevDoubleBackslash && !escapeChars.includes(char)) {
+      throw new SyntaxError();
+    } else if (char === '"' && !prevDoubleBackslash) {
+      return json.slice(start + 1, i);
+    }
+    i++;
+  }
+}
